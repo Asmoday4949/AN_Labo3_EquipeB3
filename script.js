88d677e639615a22d0ea83673ec945ae162af94d
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-04-08 21:13:18 +0200

    reset div before displaying

--- script.js
+++ script.js
@@ -19,24 +19,32 @@ function solveMatrix()
   let input = document.getElementById('matrix')
   let matrixPath = `matrix/${ input.options[input.selectedIndex].value }.json`;
 
+  //reset the div
+  document.getElementById("divOriginalMatrix").innerHTML = "";
+  document.getElementById("divOriginalVector").innerHTML = "";
+  document.getElementById("divTransformMatrix").innerHTML = "";
+  document.getElementById("divTransformVector").innerHTML = "";
+  document.getElementById("divResultsLinSys").innerHTML = "";
+
   loadMatrix(matrixPath, function(response)
   {
-    let jsonMatrix = JSON.parse(response);
-    let solver = new LinearSystemSolver(jsonMatrix);
+    let jsonMatrix = JSON.parse(response); //parse the json from the file
+    let solver = new LinearSystemSolver(jsonMatrix); //create a class made to solve linear system
     solver.display("divOriginalMatrix", "divOriginalVector");
-    let startTime = performance.now();
+    let startTime = performance.now(); //start timer
     let results = solver.solve();
-    let solveTime = performance.now() - startTime;
+    let solveTime = performance.now() - startTime; // register the end of the timer
+
     if(results !== undefined)
     {
-      solver.display("divTransformMatrix", "divTransformVector");
-      solver.displayVector(results, "divResultsLinSys")
+      solver.display("divTransformMatrix", "divTransformVector"); //display the triangular matrix and its vector
+      solver.displayVector(results, "divResultsLinSys"); //display the result
     }
     else
     {
         alert('La matrice ne peut pas être résolu');
     }
-	
+
 	document.getElementById("divTime").innerHTML = "<p>Temps de résolution : " + solveTime + "ms</p>";
   });
 }
